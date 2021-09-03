54807c3bddbf681d1068619d77362a826f5ed770
morcsanyitamas <mtomi8@gmail.com>
2021-09-03 14:24:14 +0200

    general print numbers done

--- main.py
+++ main.py
@@ -92,8 +92,38 @@ def print_nine():
     codebug.set_row(0, 0b11000)
 
 
+def print_number(number):
+    if number == 1:
+        print_one()
+    elif number == 2:
+        print_two()
+    elif number == 3:
+        print_three()
+    elif number == 4:
+        print_four()
+    elif number == 5:
+        print_five()
+    elif number == 6:
+        print_six()
+    elif number == 7:
+        print_seven()
+    elif number == 8:
+        print_eight()
+    elif number == 9:
+        print_nine()
+    elif number == 0:
+        print_zero()
+
+
 def main():
-    # number = 
+    numbers = []
+    for number in range(0, 10):
+        numbers.append(number)
+    # while True:
+    random_number = random.randint(0, 9)
+    if random_number in numbers:
+        print(random_number)
+        print_number(random_number)
     
 
 
