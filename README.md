e12b1f0d7fb76885ec5d19134c2fdaa354a0ac11
morcsanyitamas <mtomi8@gmail.com>
2021-09-29 08:51:07 +0200

    Codebug II. done

---
+++ README.md
@@ -0,0 +1,19 @@
+# Codebug I.
+## Topics
+1. Introduction to CodeBug programming
+1. Defining the software/game: *player has to choose a number between 0-9. CodeBug try to guess the number by displaying it. If the guess is correct, player has to press button A otherwise button B.*
+1. Basic implementation of the game
+1. Introduction of functions
+
+# Codebug II.
+## Topics
+1. Reading from file
+1. Complex data structures (e.g.: nested lists)
+1. Number systems in practice
+
+# Comming soon
+## CodeBug III.
+### Topics
+1. Data sturctures: dictionaries
+1. Display characters / scrolling letters
+1. Improvemen/enhancement of the game logic
--- main.py
+++ main.py
@@ -5,13 +5,40 @@ import time
 codebug = codebug_tether.CodeBug()
 
 
+def read_from_file(path):
+    characters = []
+    with open(path, 'r') as chars_file:
+        for line in chars_file:
+            data = []
+            datas = line.split(' # ')[0]
+            data = datas.split(', ')
+            number_data = [int(char, 16) for char in data]
+            characters.append(number_data)
+    return characters
+
+
+def print_character(character):
+    codebug.clear()
+    character.append(0)
+    character.insert(0, 0)
+    for col_index in range(0, 5):
+        codebug.set_col(col_index, character[col_index] >> 3)
+
+
+def print_number(number):
+    charaters = read_from_file('font.txt')
+    char_number = charaters[16 + number]
+    print_character(char_number)
+    
+
 def print_zero():
     codebug.clear()
-    codebug.set_row(4, 0b00100)
-    codebug.set_row(3, 0b01010)
-    codebug.set_row(2, 0b01010)
-    codebug.set_row(1, 0b01010)
-    codebug.set_row(0, 0b00100)
+    codebug.set_row(4, 0x04)
+    codebug.set_row(3, 0x0A)
+    codebug.set_row(2, 0x0A)
+    codebug.set_row(1, 0x0A)
+    codebug.set_row(0, 0x04)
+
 
 def print_one():
     codebug.clear()
@@ -20,6 +47,7 @@ def print_one():
     codebug.set_row(2, 0b00100)
     codebug.set_row(1, 0b00100)
     codebug.set_row(0, 0b00100)
+
 
 def print_five():
     codebug.clear()
@@ -93,27 +121,27 @@ def print_nine():
     codebug.set_row(0, 0b11000)
 
 
-def print_number(number):
-    if number == 1:
-        print_one()
-    elif number == 2:
-        print_two()
-    elif number == 3:
-        print_three()
-    elif number == 4:
-        print_four()
-    elif number == 5:
-        print_five()
-    elif number == 6:
-        print_six()
-    elif number == 7:
-        print_seven()
-    elif number == 8:
-        print_eight()
-    elif number == 9:
-        print_nine()
-    elif number == 0:
-        print_zero()
+# def print_number(number):
+#     if number == 1:
+#         print_one()
+#     elif number == 2:
+#         print_two()
+#     elif number == 3:
+#         print_three()
+#     elif number == 4:
+#         print_four()
+#     elif number == 5:
+#         print_five()
+#     elif number == 6:
+#         print_six()
+#     elif number == 7:
+#         print_seven()
+#     elif number == 8:
+#         print_eight()
+#     elif number == 9:
+#         print_nine()
+#     elif number == 0:
+#         print_zero()
 
 
 def waiting_for_feedback():
@@ -148,4 +176,5 @@ def main():
     print('You win!')
 
 
-main()
+# main()
+print_number(9)
